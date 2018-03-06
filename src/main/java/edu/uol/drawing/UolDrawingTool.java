ce57aa5ddd091328a6533fdb1c5fbe4ab0bfcaf6
Damian Chambers <damian.chambers@yahoo.com>
2018-03-06 09:13:12 -0500

    UI Toolbar

--- src/main/java/edu/uol/drawing/UolDrawingTool.java
+++ src/main/java/edu/uol/drawing/UolDrawingTool.java
@@ -1,6 +1,7 @@
 
 package edu.uol.drawing;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Frame;
@@ -13,6 +14,8 @@ import java.awt.Panel;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
@@ -23,7 +26,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JColorChooser;
+import javax.swing.JFrame;
+import javax.swing.JRadioButton;
+import javax.swing.JToolBar;
 
 import edu.uol.drawing.ShapeFactory.ShapeTypes;
 import edu.uol.drawing.shapes.FillColorable;
@@ -31,14 +39,26 @@ import edu.uol.drawing.shapes.OurShape;
 import edu.uol.drawing.shapes.OutlineColorable;
 import edu.uol.drawing.shapes.Selectable;
 
-public class UolDrawingTool extends Frame {
+public class UolDrawingTool extends JFrame {
 
 	private static final String EXIT_MENU_LABEL = "Exit";
 	private static final String SET_OUTLINE_COLOR_MENU_LABEL = "Set Outline Color...";
 	private static final String SET_FILL_COLOR_MENU_LABEL = "Set Fill Color...";
+	
+	private static final String FILL_COLOR_LABEL = "Fill Color";
+	private static final String OUTLINE_COLOR_LABEL = "Outline Color";
+	
+	
 	private DrawingPanel panel;
 	private Color fillColor = Color.GREEN;
 	private Color outlineColor = Color.BLACK;
+	
+	
+	
+	private JButton fillColorButton;
+	private JButton outlineColorButton;
+	
+	private JToolBar colorToolBar;
 
 	public UolDrawingTool() {
 		// set frame's title
@@ -47,14 +67,71 @@ public class UolDrawingTool extends Frame {
 		addMenu();
 		// add drawing panel
 		addPanel();
+		
+		//add toolbar
+		addToolBar();
+		
 		// add window listener
 		this.addWindowListener(new WindowHandler());
 		// set frame size
-		this.setSize(400, 400);
+		this.setSize(600, 600);
+		//center on screen
+		this.setLocationRelativeTo(null);
+		
 		// make this frame visible
 		this.setVisible(true);
 	}
 
+	private void setFillColor(Color color) {
+		this.fillColor = color;
+		
+		//set button color too.
+		fillColorButton.setBackground(color);
+	}
+	
+	private void setOutlineColor(Color color) {
+		this.outlineColor = color;
+		
+		outlineColorButton.setBackground(color);
+	}
+	
+	private void addToolBar() {
+		ButtonGroup shapeButtonGroup = new ButtonGroup();		
+		
+		fillColorButton = new JButton(SET_FILL_COLOR_MENU_LABEL);
+		outlineColorButton = new JButton(SET_OUTLINE_COLOR_MENU_LABEL);			
+				
+		fillColorButton.setActionCommand(SET_FILL_COLOR_MENU_LABEL);
+		outlineColorButton.setActionCommand(SET_OUTLINE_COLOR_MENU_LABEL);
+		
+		fillColorButton.addActionListener(new WindowHandler());
+		outlineColorButton.addActionListener(new WindowHandler());
+		
+		colorToolBar = new JToolBar();
+		
+		for (int i = 0; i < ShapeTypes.values().length; i++) {		
+			
+			
+			JRadioButton j1 = new JRadioButton(ShapeTypes.values()[i].name());
+			j1.addActionListener(new WindowHandler());
+			shapeButtonGroup.add(j1);		
+			
+			colorToolBar.add(j1);
+		}
+		
+		
+		//set default colors.
+		setFillColor(Color.RED);
+		setOutlineColor(Color.BLACK);
+		
+		colorToolBar.add(fillColorButton);
+		colorToolBar.add(outlineColorButton);
+		
+		//add toolbar to the top of window.
+		this.add(colorToolBar, BorderLayout.NORTH);
+	}
+	
+	
 	/**
 	 * This method creates menu bar and menu items and then attach the menu bar with
 	 * the frame of this drawing tool.
@@ -97,26 +174,29 @@ public class UolDrawingTool extends Frame {
 		// add mouse listener. Panel itself will be handling mouse events
 		panel.addMouseListener(panel);
 		panel.addMouseMotionListener(panel);
-		this.add(panel);
+		this.add(panel, BorderLayout.CENTER);
 	}
 
+	
 	private class WindowHandler extends WindowAdapter implements ActionListener {
 		public void windowClosing(WindowEvent e) {
 			System.exit(0);
 		}
 
 		public void actionPerformed(ActionEvent e) {
+			
+			
 			if (EXIT_MENU_LABEL.equals(e.getActionCommand())) {
 				System.exit(0);
 			} else if (SET_FILL_COLOR_MENU_LABEL.equals(e.getActionCommand())) {
 				Color newColor = JColorChooser.showDialog(panel, "Choose Fill Color", fillColor);
 				if (newColor != null) {
-					fillColor = newColor;
+					setFillColor(newColor);
 				}
 			} else if (SET_OUTLINE_COLOR_MENU_LABEL.equals(e.getActionCommand())) {
 				Color newColor = JColorChooser.showDialog(panel, "Choose Outline Color", outlineColor);
 				if (newColor != null) {
-					outlineColor = newColor;
+					setOutlineColor(newColor);
 					System.out.println(outlineColor.toString());
 				}
 			} else {
@@ -141,7 +221,7 @@ public class UolDrawingTool extends Frame {
 				}
 
 			}
-		}
+		}		
 	}
 }
 
